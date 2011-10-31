18931406f25c8701a921c971638ca54c4adf8464
stdusr <stdusr@box.(none)>
2011-10-31 11:52:05 +0100

    Added plotting capabilities.

--- src/input/NumberReader.java
+++ src/input/NumberReader.java
@@ -12,7 +12,7 @@ import data.Number;
 public class NumberReader {
 
 	private File input;
-	
+
 	private List<Number> data;
 
 	public NumberReader() {
@@ -40,12 +40,13 @@ public class NumberReader {
 		try {
 			String lString = "";
 			while ((lString = fReader.readLine()) != null) {
-				String[] tokens = lString.split("\t");
-				int[] points = new int[tokens.length-1];
-				for (int i = 0; i < tokens.length; i++) {
+				String[] tokens = lString.trim().split("\\s+");
+				int[] points = new int[tokens.length - 1];
+				for (int i = 0; i < tokens.length - 1; i++) {
 					points[i] = Integer.parseInt(tokens[i]);
 				}
-				Number parsedNumber = new Number(Integer.parseInt(tokens[tokens.length]));
+				Number parsedNumber = new Number(
+						Integer.parseInt(tokens[tokens.length - 1]));
 				parsedNumber.setPoints(points);
 				data.add(parsedNumber);
 			}
@@ -53,5 +54,9 @@ public class NumberReader {
 			System.out.println("File could not be read!");
 			e.printStackTrace();
 		}
+	}
+
+	public List<Number> getData() {
+		return this.data;
 	}
 }
